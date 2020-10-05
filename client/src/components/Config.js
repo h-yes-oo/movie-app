6b89d1bb3b4d6ace21a152c80faf74bf88dab238
h-yes-oo <hyesoo5115@naver.com>
2020-10-05 22:51:48 +0900

    start with boilerplate

---
+++ client/src/components/Config.js
@@ -0,0 +1,5 @@
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '6e093a50c9aeca5a9ccf1bc778d74327';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
