4ae383a07f4672a3682ea65112bc67f34c6cbf79
h-yes-oo <hyesoo5115@naver.com>
2020-11-27 11:24:15 +0900

    Actor Toggle View

--- client/src/components/views/MovieDetail/MovieDetail.js
+++ client/src/components/views/MovieDetail/MovieDetail.js
@@ -3,11 +3,16 @@ import {API_URL,API_KEY} from '../../Config'
 import MainImage from '../LandingPage/Sections/MainImage'
 import { IMAGE_BASE_URL } from "../../Config"
 import MovieInfo from "./Sections/MovieInfo"
+import GridCards from "../commons/GridCards"
+import {Row} from 'antd'
+
 
 function MovieDetail(props) {
 
     let movieId = props.match.params.movieId
     const [Movie,setMovie] = useState([])
+    const [Casts,setCasts] = useState([])
+    const [ActorToggle, setActorToggle] = useState(false)
 
     useEffect(()=>{
 
@@ -18,10 +23,19 @@ function MovieDetail(props) {
         fetch(endpointInfo)
         .then(response => response.json())
         .then(response => {
-            console.log(response)
             setMovie(response)
         })
+
+        fetch(endpointCrew)
+        .then(response => response.json())
+        .then(response=> {
+            setCasts(response.cast)
+        })
     },[])
+
+    const toggleActorView = () => {
+        setActorToggle(!ActorToggle);
+    }
 
     return (
         <div>
@@ -36,13 +50,30 @@ function MovieDetail(props) {
             <div style={{ width: '85%', margin: '1rem auto'}}>
                 {/*Movie Info*/}
                 <MovieInfo movie={Movie}/>
+
                 <br/>
+
                 {/* Actors Grid */}
+
                 <div style={{ display: 'flex', justifyContent: 'center', margin: '2rem'}}>
-                    <button> Toggle Actor View </button>
+                    <button onClick = {toggleActorView}> Toggle Actor View </button>
                 </div>
+                { ActorToggle &&
+                <Row gutter={[16,16]}>
+                    {Casts && Casts.map((cast, index) => (
+                        <React.Fragment key={index}>
+                            <GridCards
+                                image = {cast.profile_path ? 
+                                `${IMAGE_BASE_URL}w500${cast.profile_path}` : null
+                                }
+                                characterName = {cast.name}
+                            />
+                        </React.Fragment>
+                    ))}
+                </Row>
+                }
             </div>
-            
+
         </div>
     )
 }
--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -2,17 +2,28 @@ import React from 'react'
 import { Col } from 'antd';
 
 function GridCards(props) {
-    return (
-        //반응형을 만들기 위해
-        //large 일때는 column이 6개 이고, 따라서 row가 24/6=4인 형태로
+
+    if(props.landingPage){
+        return(
         <Col lg={6} md={8} xs={24}>
-            <div style={{position: 'relative'}}>
-                <a href={`/movie/${props.movieId}`}>
-                    <img style = {{ width: '100%', height: '320px'}} src={props.image} alt={props.movieName} />
-                </a>
-            </div>
-        </Col>
-    )
+                <div style={{position: 'relative'}}>
+                    <a href={`/movie/${props.movieId}`}>
+                        <img style = {{ width: '100%', height: '320px'}} src={props.image} alt={props.movieName} />
+                    </a>
+                </div>
+            </Col>
+        )
+    } else{
+        return (
+            //반응형을 만들기 위해
+            //large 일때는 column이 6개 이고, 따라서 row가 24/6=4인 형태로
+            <Col lg={6} md={8} xs={24}>
+                <div style={{position: 'relative'}}>
+                    <img style = {{ width: '100%', height: '320px'}} src={props.image} alt={props.characterName} />
+                </div>
+            </Col>
+        )
+    }
 }
 
 export default GridCards
